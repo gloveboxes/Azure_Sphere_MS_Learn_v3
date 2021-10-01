265acd198b166101776313c7e6f99647aad6b741
Dave Glover <dglover@microsoft.com>
2021-10-01 12:41:18 +1000

    Updated to build labs
    backs up existing azsphere_board.txt before build all process

--- build_labs.ps1
+++ build_labs.ps1
@@ -1,5 +1,9 @@
 $StartTime = $(get-date)
 
+if (!(Test-Path azsphere_board.txt.backup)) {
+    Copy-Item -Path azsphere_board.txt -Destination azsphere_board.txt.backup
+}
+
 'set(AVNET TRUE "AVNET Azure Sphere Starter Kit Revision 1")' | Out-File -FilePath azsphere_board.txt
 Write-Output "`n=================Building for Azure Sphere Developer Board========================="
 Get-Content ./azsphere_board.txt
@@ -30,7 +34,12 @@ if ($?) {
     pwsh ./Tools\build-tools\build_all.ps1
 }
 
-Remove-Item ./azsphere_board.txt
+Remove-Item azsphere_board.txt
+
+if (Test-Path azsphere_board.txt.backup) {
+    Copy-Item -Path azsphere_board.txt.backup -Destination azsphere_board.txt
+    Remove-Item azsphere_board.txt.backup
+}
 
 $elapsedTime = $(get-date) - $StartTime
 $totalTime = "{0:HH:mm:ss}" -f ([datetime]$elapsedTime.Ticks)
